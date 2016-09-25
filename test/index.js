c86604014499b7695d0b6d482a8635e886fede5c
Jai Kishan <kishan.jai1991@gmail.com>
2016-09-25 19:19:29 +0530

    add database event test

--- test/index.js
+++ test/index.js
@@ -11,6 +11,7 @@ const it = lab.it;
 
 const Streams = require('./fixture/streams');
 const Server = require('./fixture/server');
+const Database = require('./fixture/database');
 
 const Console = require('..');
 
@@ -82,6 +83,78 @@ describe('ProteusjsConsole', () => {
 
         expect(out.data).to.have.length(1);
         expect(out.data[0]).to.be.equal('160925/131338.210, [server:response] http://localhost:6200: \u001b[1;32mget\u001b[0m /hapiget {} \u001b[32m200\u001b[0m (52ms)\n');
+        done();
+      });
+    });
+
+  });
+
+  describe('Database Events', () => {
+
+    it('returns a formatted string for "query" events', { plan: 2 }, (done) => {
+
+      const reporter = new Console();
+      const out = new Streams.Writer();
+      const reader = new Streams.Reader();
+
+      reader.pipe(reporter).pipe(out);
+      reader.push(Database.query);
+      reader.push(null);
+      reader.once('end', () => {
+
+        expect(out.data).to.have.length(1);
+        expect(out.data[0]).to.be.equal('160925/133101.545, [knex:query] \u001b[1;32mselect\u001b[0m [ sql: select * from `log` ]\n');
+        done();
+      });
+    });
+
+    it('returns a formatted string for "end" events', { plan: 2 }, (done) => {
+
+      const reporter = new Console();
+      const out = new Streams.Writer();
+      const reader = new Streams.Reader();
+
+      reader.pipe(reporter).pipe(out);
+      reader.push(Database.end);
+      reader.push(null);
+      reader.once('end', () => {
+
+        expect(out.data).to.have.length(1);
+        expect(out.data[0]).to.be.equal('160925/133101.552, [knex:end] \u001b[1;32minfo\u001b[0m: Query executed successfully\n');
+        done();
+      });
+    });
+
+    it('returns a formatted string for "query error" events', { plan: 2 }, (done) => {
+
+      const reporter = new Console();
+      const out = new Streams.Writer();
+      const reader = new Streams.Reader();
+
+      reader.pipe(reporter).pipe(out);
+      reader.push(Database.queryError);
+      reader.push(null);
+      reader.once('end', () => {
+
+        expect(out.data).to.have.length(1);
+        expect(out.data[0]).to.be.equal('160925/134134.738, [knex:queryerror] \u001b[1;31merror\u001b[0m: [ select * from `logg` - ER_NO_SUCH_TABLE: Table \'sample.logg\' doesn\'t exist ]\n');
+        done();
+      });
+    });
+
+    it('returns a formatted string for "error" events', { plan: 2 }, (done) => {
+
+      const reporter = new Console();
+      const out = new Streams.Writer();
+      const reader = new Streams.Reader();
+
+      reader.pipe(reporter).pipe(out);
+      reader.push(Database.error);
+      reader.push(null);
+      reader.once('end', () => {
+
+        expect(out.data).to.have.length(1);
+        expect(out.data[0]).to.be.equal('160925/134134.741, [knex:error] \u001b[1;31merror\u001b[0m: [ select * from `logg` - ER_NO_SUCH_TABLE: Table \'sample.logg\' doesn\'t exist ]\n');
         done();
       });
     });
